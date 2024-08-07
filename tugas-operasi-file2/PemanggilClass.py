7ac4e0c540f9697fa8d6c2e501c6a93bd51cdb09
sellamutomimah <sellamutomimah@gmail.com>
2024-08-07 11:42:48 +0700

    tugas2

---
+++ tugas-operasi-file2/PemanggilClass.py
@@ -0,0 +1,7 @@
+from FilClass import HandleFile
+
+file = HandleFile()
+
+file.baca_file('Output.txt')
+
+file.tulis_files('Output.txt', 'a') # Bisa Di Gani Mode Sesuka Hati "TULIS_FILE("nama_file", "mode")"
